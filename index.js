12becc77021e867335f7451f308123707df02d16
Sharon <sharonvcardozom.com>
2020-04-09 13:12:06 -0500

    Se agrego todo

---
+++ index.js
@@ -0,0 +1,52 @@
+/* var n1=2;
+var n2=2;
+var n3="perro";
+var i=0;
+var array=["casa",1,2,"perro"];
+
+if (n1==n2) {
+    //console.log ("Son iguales")   
+    //name(5,7)
+    var result=name(5,7);
+    console.log(result);
+}else {
+    console.log ("No son iguales")
+}
+
+/* switch (n3) {
+    case "casa": 
+        console.log ("El valor es "+n3)
+        break;
+    case "perro":
+        console.log ("El valor es "+n3)
+    default:
+        break; */
+
+//while (i<=10) {
+   // console.log (i);
+    //i++}
+
+
+/* for (let i = 0; i < array.length; i++) {
+        
+    console.log (array[i])
+    
+}
+//función
+
+(function name(numero1, numero2) {
+    return numero1+numero2 
+
+})() */
+
+//2da
+
+var bottonFirst= document.getElementsByClassName("bottonFirst");
+    console.log (bottonFirst)
+
+bottonFirst[0].addEventListener("click",descarga)
+
+function descarga() {
+    alert("descargando")
+}
+
